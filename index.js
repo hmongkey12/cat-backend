5a88bea63b732fdcee44a30e210c913fcc41634f
Khammadao Lee <khammadaolee@Khammadaos-MacBook-Pro.local>
2022-05-05 08:25:46 -0700

    updated response, removed no cors

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 require("dotenv").config();
 const cors = require("cors");
-const { application } = require("express");
 const express = require("express");
 const server = express();
 const PORT = process.env.PORT || 3000;
 
 const { middleRouter } = require("./middlewares/middleware");
 
-application.use(cors());
+server.use(cors());
 server.use("/users", middleRouter);
 
 server.listen(PORT, (err) => {
