25a0338411f630f58007ca9c1a77cd1d37632016
Khammadao Lee <khammadaolee@Khammadaos-MacBook-Pro.local>
2022-05-05 00:34:12 -0700

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
+const cors = require("cors");
+const { application } = require("express");
 const express = require("express");
 const server = express();
 const PORT = process.env.PORT || 3000;
 
 const { middleRouter } = require("./middlewares/middleware");
 
+application.use(cors());
 server.use("/users", middleRouter);
 
 server.listen(PORT, (err) => {
